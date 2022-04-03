deb3c3dd59b6e94942caca7612dc2ca3d015ed75
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 13:39:02 +0800

    2022 is not a leap year

--- compoundIf.go
+++ compoundIf.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
+func isLeapYear(year int) bool {
+	if (year%400 == 0) || (year%4 == 0 && year%100 != 0) {
+		fmt.Printf("%v is a leap year", year)
+		return true
+	} else {
+		fmt.Printf("%v is not a leap year", year)
+		return false
+	}
+}
+
 func main() {
-	//Insert your code here
+	var year int
+	fmt.Println("Give me the year")
+	fmt.Scanln(&year)
+
+	isLeapYear(year)
 }
